d6106d8259a9a4ae23fc3c750cfcd72474d51333
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-07 08:12:46 +0530

    sm8350-common: dolby: Start services as early as possible on bootup
    BOOT_COMPLETED intent is broadcast only after the device is unlocked and
    user data (encrypted storage) is available. Use LOCKED_BOOT_COMPLETED
    intent for services that don't require accessing data.
    
    While we're at it, target latest SDK and uprev the package.

--- dolby/src/com/xiaomi/dolby/BootCompletedReceiver.java
+++ dolby/src/com/xiaomi/dolby/BootCompletedReceiver.java
@@ -16,14 +16,27 @@ import com.xiaomi.dolby.DolbyUtils;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final String TAG = "XiaomiDolby";
-    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            return;
+        Log.i(TAG, "Received intent: " + intent.getAction());
+
+        switch (intent.getAction()) {
+            case Intent.ACTION_LOCKED_BOOT_COMPLETED:
+                onLockedBootCompleted(context);
+                break;
+            case Intent.ACTION_BOOT_COMPLETED:
+                onBootCompleted(context);
+                break;
         }
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+    }
+
+    private static void onLockedBootCompleted(Context context) {
+        // Services that don't require reading from data.
+    }
+
+    private static void onBootCompleted(Context context) {
+        // Data is now accessible (user has just unlocked).
         DolbyUtils.getInstance(context).onBootCompleted();
     }
 }
