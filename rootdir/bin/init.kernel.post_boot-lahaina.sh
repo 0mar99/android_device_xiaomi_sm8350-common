d215fb7a49b4d76f0b47f00d231480c0e74f4a37
Jake Weinstein <jake@aospa.co>
2023-11-28 04:53:19 +0000

    sm8350-common: rootdir: Make less cores available for background tasks
    With SurfaceFlinger and HWC on the foreground cpuset,
    important rendering tasks are moved off of the background
    cpusets, so let's reserve one little core for foreground
    and top-app.
    
    Change-Id: I4f0982f3ff4dce756095667df0d08f18817b896a

--- rootdir/bin/init.kernel.post_boot-lahaina.sh
+++ rootdir/bin/init.kernel.post_boot-lahaina.sh
@@ -193,7 +193,7 @@ echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
-echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-2 > /dev/cpuset/system-background/cpus
 echo 0-6 > /dev/cpuset/foreground/cpus
 
 # configure governor settings for silver cluster
