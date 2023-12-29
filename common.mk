4a2948d413b79ff62d955cfff682973a9af11aa3
0mar99 <omarantonio9972@gmail.com>
2023-12-28 23:13:54 -0600

    sm8350-common: Orion-ify!

--- common.mk
+++ common.mk
@@ -481,7 +481,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-orion
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
