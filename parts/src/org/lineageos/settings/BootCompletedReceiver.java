7c9c0f2c2e4411b3127c8d4fa2bbf0eec78eff46
AnierinB <anierin@evolution-x.org>
2024-01-16 01:13:55 +0300

    sm8350-common: parts: Use directBootAware
    - This way settings can be restored prior to unlocking after reboot.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -21,7 +21,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+        if (!intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
             return;
         }
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
