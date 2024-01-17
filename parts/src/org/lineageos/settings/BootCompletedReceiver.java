84b11631c224ff7283a04fd8c86a42e33f28080c
TheScarastic <warabhishek@gmail.com>
2024-01-17 13:58:14 +0300

    sm8350-common: parts: Add dynamic thermal profile implementation
    sm8350-common: parts: Make thermal profiles naming completely generic
    
    Drop the reference to YouTube because that's product-specific.
    Moreover, the purpose of such thermal profile should be video streaming,
    so name it in that generic way.
    
    sm8350-common: parts: Switch to TaskStackListener
    
    sm8350-common: parts: Refactor whole code
    
    sm8350-common: parts: Check if kernel support Thermal Profiles
    
    sm8350-common: parts: Use correct gaming thermal
    
    * thermal-pubg.conf is missing which is selected with 13, instead we have thermal-tgame.conf which is 9.
    * thanks amack766 for informig.
    
    sm8350-common: parts: Add {navigation,video} thermal profiles

---
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2023 LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.lineageos.settings.thermal.ThermalUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "XiaomiParts";
+    private static final boolean DEBUG = true;
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+
+        // Thermal
+        ThermalUtils.startService(context);
+    }
+}
