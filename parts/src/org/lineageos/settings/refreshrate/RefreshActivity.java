bd4a76150a99bb62adc95581c210251617877d31
AnierinB <anierin@evolution-x.org>
2024-03-23 23:15:30 +0530

    sm8350-common: parts: Move to PreferenceFragmentCompat PreferenceFragment was deprecated in API level 28.
    https: //developer.android.com/jetpack/androidx/releases/preference#1.1.0
    Co-Authored-By: drkphnx <dark.phnx12@gmail.com>
    Co-Authored-By: TheScarastic <warabhishek@gmail.com>
    Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-By: johnmart19 <johnivan19999@gmail.com>
    Co-Authored-By: someone5678 <nemui3353@gmail.com>

--- parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The LineageOS Project
+ * Copyright (C) 2023-2024 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ public class RefreshActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+        getSupportFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new RefreshSettingsFragment(), TAG_REFRESH).commit();
     }
 
