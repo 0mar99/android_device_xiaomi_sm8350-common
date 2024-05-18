1cee6c9988471fe24c35a3a82d9211d9943f65d6
Arian <arian.kulmer@web.de>
2024-05-18 15:52:45 +0200

    sm8350-common: Provide a static lib for power mode extension
    Signed-off-by: 0mar99 <omarag9099@gmail.com>
    Change-Id: I3e2cf17bf1e733838e9e18afdf3f888e814593dc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -141,7 +141,7 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_BOARD_PLATFORM := lahaina
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(COMMON_PATH):libperfmgr-ext-xiaomi
 
 # PowerShare
 TARGET_POWERSHARE_PATH := /sys/class/qcom-battery/reverse_chg_mode
