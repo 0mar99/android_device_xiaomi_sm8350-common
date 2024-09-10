f94920b22d65f0c438ecd094b1b1baf04c2a7063
Kyuofox <kyuofox@gmail.com>
2024-09-10 10:27:25 +0800

    sm8350-common: Allow gettid call for atfwd
    We never know what upstream is thinking:
    W ATFWD-daemon: libminijail[4756]: failed to get path of fd 3: No such file or directory
    W ATFWD-daemon: libminijail[4756]: allowing syscall: connect
    W ATFWD-daemon: libminijail[4756]: allowing syscall: fcntl
    W ATFWD-daemon: libminijail[4756]: allowing syscall: sendto
    W ATFWD-daemon: libminijail[4756]: allowing syscall: socket
    W ATFWD-daemon: libminijail[4756]: allowing syscall: writev
    W ATFWD-daemon: libminijail[4756]: logging seccomp filter failures
    E ATFWD-daemon: libminijail[4756]: blocked syscall: gettid
    
    Change-Id: I42e638f9f25395b37df57471fce68cc73e11e83f
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -77,6 +77,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "/max_retry_alloc_output_timeout/ s/10000/0/" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            echo 'gettid: 1' >> ${2}
+            ;;
         vendor/lib/c2.dolby.client.so)
             "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
             ;;
